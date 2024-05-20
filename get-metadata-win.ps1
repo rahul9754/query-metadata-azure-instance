c6de512d21c73f4069b3036a1751f1dec86d7597
rahulgupta9270 <rahulgupta12786@gmail.com>
2024-05-20 15:59:56 +0530

    optimizing cmd

--- get-metadata-win.ps1
+++ get-metadata-win.ps1
@@ -1,12 +1,8 @@
 # This script retrieves the Azure Instance Metadata of Azure Windows VM
 # and extracts specific values from the JSON response
 
-# Define the metadata service URL and API version
-$metadataUrl = "http://169.254.169.254/metadata/instance"
-$apiVersion = "2021-02-01"
-
 # Send a GET request to the metadata service and store the response
-$response = Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -Uri "$metadataUrl?api-version=$apiVersion"
+$response = Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -Uri "http://169.254.169.254/metadata/instance?api-version=2021-02-01"
 
 # Extract the desired value (location) from the response
 $reqValue = $response.compute.location
